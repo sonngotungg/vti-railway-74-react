ab1cef0ae3e45f5028a6cc3bf683fff13b83e37d
QuangChinh1 <chinh.v1907@gmail.com>
2023-11-01 23:48:31 +0900

    1

--- lesson3/first-react-app/src/App.js
+++ lesson3/first-react-app/src/App.js
@@ -1,42 +1,87 @@
-import "./App.css";
-import { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-function App() {
-    // const [count, setCount] = useState(0);
-    // return (
-    //     <div className="App">
-    //         <p>{count}</p>
-    //         <button onClick={() => setCount(count + 1)}>Add</button>
-    //         <button onClick={() => setCount(count - 1)}>Subtract</button>
-    //     </div>
-    // );
-    const [showList, setShowList] = useState(false);
-    const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+function StudentForm({ onAddStudent }) {
+    const nameRef = useRef(null);
+    const rollNumberRef = useRef(null);
+    const gradeRef = useRef(null);
+
+    useEffect(() => {
+        nameRef.current.focus(); // Automatically focus the first input field on component mount
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const name = nameRef.current.value;
+        const rollNumber = rollNumberRef.current.value;
+        const grade = gradeRef.current.value;
+
+        if (name && rollNumber && grade) {
+            const newStudent = {
+                name,
+                rollNumber,
+                grade,
+            };
+
+            onAddStudent(newStudent);
+
+            // Clear input fields
+            nameRef.current.value = "";
+            rollNumberRef.current.value = "";
+            gradeRef.current.value = "";
+        }
+    };
 
     return (
         <div>
-            <h1>
-                Conditional Rendering, Conditional Styling, and Rendering a List
-            </h1>
-            <button onClick={() => setShowList(!showList)}>Toggle List</button>
-            {showList && (
-                <ul className="list">
-                    {items.map((item, index) => {
-                        return (
-                            <li
-                                key={index}
-                                className={
-                                    index % 2 === 0
-                                        ? "list-item-even"
-                                        : "list-item-odd"
-                                }
-                            >
-                                {item}
-                            </li>
-                        );
-                    })}
-                </ul>
-            )}
+            <h2>Enter Student Information</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" ref={nameRef} required />
+                </label>
+                <label>
+                    Roll Number:
+                    <input type="text" ref={rollNumberRef} required />
+                </label>
+                <label>
+                    Grade:
+                    <input type="text" ref={gradeRef} required />
+                </label>
+                <button type="submit">Add Student</button>
+            </form>
+        </div>
+    );
+}
+
+function StudentList({ students }) {
+    return (
+        <div>
+            <h2>Student List</h2>
+            <ul>
+                {students.map((student, index) => (
+                    <li key={index}>
+                        Name: {student.name}, Roll Number: {student.rollNumber},
+                        Grade: {student.grade}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function App() {
+    const [students, setStudents] = useState([]);
+
+    const handleAddStudent = (newStudent) => {
+        setStudents([...students, newStudent]);
+    };
+
+    return (
+        <div className="app">
+            <h1>Student Information</h1>
+            <StudentForm onAddStudent={handleAddStudent} />
+            <StudentList students={students} />
         </div>
     );
 }
--- lesson4/react-router-dom/main.js
+++ lesson4/react-router-dom/main.js
@@ -6,9 +6,9 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
