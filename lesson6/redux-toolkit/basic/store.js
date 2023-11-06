6c1a32a09de1577e7e7cc2a6bf56b8ccd6808a10
sonngotungg <son.ngotung160@gmail.com>
2023-11-06 21:33:53 +0700

    add redux-toolkit example

---
+++ lesson6/redux-toolkit/basic/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todosSlice from './todosSlice';
+import counterSlice from './counterSlice'; // Import the counterSlice
+
+const store = configureStore({
+  reducer: {
+    todos: todosSlice,
+    counter: counterSlice, // Add the counterSlice
+  },
+});
+
+export default store;
